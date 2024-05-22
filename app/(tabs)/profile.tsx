55202a444c3d9808e156bd1c73aaa160557a1314
Jason Zhou <jasonzhou@Jasons-MacBook-Pro.local>
2024-05-22 17:26:12 +0800

    creating login screen frontend

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,43 @@
+import {View,Text,StyleSheet,Image} from 'react-native'
+
+export default function AuthenticateScreen(){
+        return(
+            <View style={styles.mainContainer}>
+                <View>
+                    <Text style={styles.text}>
+                        Hungry? Let's get started!
+                    </Text>
+                    <View style={styles.imageContainer}>
+                        <Image source={require('@/assets/images/eating.jpeg')}
+                        style={styles.image}/>
+                        
+                    </View>
+                </View>
+            </View>
+        )
+}
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        backgroundColor:'#FFE8C8',
+        flex:1,
+
+    },
+    secondContainer:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        backgroundColor:'#FFE8C8',
+    },
+    text:{
+        marginTop:100,
+        alignContent:'center'
+    },
+    imageContainer:{
+        marginTop:50,
+        flexDirection:'row',
+    },
+    image:{
+        width:350,
+        height:350
+    }
+})
